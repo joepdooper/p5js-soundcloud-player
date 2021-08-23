371943ce4c6a52e0d7c90ede24db690db86aaa8e
Joep Dooper <joepdooper@hotmail.com>
2021-08-23 22:46:00 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -17,7 +17,7 @@ var end = false;
 var seconds, minutes, bass, mid, treble, list, ntgr;
 
 //SoundCloud
-var CLIENT_ID = '95f22ed54a5c297b1c41f72d713623ef';
+var CLIENT_ID = 'GET-YOURS-AT-SOUNDCLOUD';
 var PLAYLIST_URL = 'https://soundcloud.com/fftb/sets/party';
 SC.initialize({
   client_id: CLIENT_ID
